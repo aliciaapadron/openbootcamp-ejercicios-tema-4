793b20c5b71b05627aa945e1867c806a364df1d4
Alicia Padrón <aliciaapadron@gmail.com>
2022-07-06 13:19:24 +0200

    add exercise with WHILE

---
+++ while.java
@@ -0,0 +1,13 @@
+//Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3
+package main;
+
+class Main {
+	public static void main(String[] args) {
+	   int numeroWhile = 1;
+
+		while (numeroWhile <= 3){
+        System.out.println(numeroWhile);
+        numeroWhile += 1;
+	}
+	}
+}
